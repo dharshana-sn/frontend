c84a8e9245c70c2a3a314db15946650281f6f50a
dharshu_ns <dharshanans54@gmail.com>
2025-04-03 22:02:30 +0530

    implemented createBooks

--- src/pages/CreateBooks.jsx
+++ src/pages/CreateBooks.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react';
+import BackButton from '../components/BackButton';
+import axios from 'axios';
+import Spinner from '../components/spinner';
+import { useNavigate } from 'react-router-dom';
 
 const CreateBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPulishYear] = useState('');
+  const [loading, setLoading] = useState('');
+  const navigate = useNavigate();
+  const handleSaveBook=()=>{
+    const data ={
+      title,
+      author,
+      publishYear
+    }
+    setLoading(true);
+    axios
+    .post('http://loacalhost:4444/books', data)
+    .then(()=>{
+      setLoading(false);
+      navigate('/');
+    })
+    .catch((error)=>{
+      setLoading(false);
+      alert('something happend please check the console for more infor');
+      console.log(error);
+    })
+  }
   return (
     <div>CreateBooks</div>
   )
