41afd3719f06aee2697ff08eafa5b211c38da7b6
dharshu_ns <dharshanans54@gmail.com>
2025-04-03 22:01:54 +0530

    updated BackButton

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { BsArrowLeft } from "react-icons/bs";
+import React from 'react';
+import { BsArrowLeft } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
-const BackButton = ({ destination = "/" }) => {
+const BackButton = ({ destination = '/' }) => {
   return (
-    <div className="flex">
+    <div className='flex'>
       <Link
         to={destination}
-        className="bg-sky-006 text-white px-4 py-1 rounded-lg w-fit"
+        className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
       >
-        <BsArrowLeft className="text-2x1" />
+        <BsArrowLeft className='text-2xl' />
       </Link>
     </div>
   );
