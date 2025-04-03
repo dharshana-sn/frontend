c3c7a446583e79667be5195b8d13ce45c4f5a3b2
dharshu_ns <dharshanans54@gmail.com>
2025-04-03 22:02:15 +0530

    Changed the destination part

--- src/pages/ShowBooks.jsx
+++ src/pages/ShowBooks.jsx
@@ -28,7 +28,7 @@ const ShowBooks = () => {
 
   return (
     <div className='p-4'>
-      <BackButton />
+      <BackButton destination='/' />
       <h1 className='text-3xl my-4'>Show Book</h1>
       {loading ? (<Spinner />) :( 
         <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
