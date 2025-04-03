f6156731daa05f3299acc4a1f6e2ae26f107d695
dharshu_ns <dharshanans54@gmail.com>
2025-04-04 01:28:25 +0530

    Updated the CreateBooks page

--- src/pages/CreateBooks.jsx
+++ src/pages/CreateBooks.jsx
@@ -1,37 +1,76 @@
-import React, {useState} from 'react';
-import BackButton from '../components/BackButton';
-import axios from 'axios';
-import Spinner from '../components/spinner';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
+import axios from "axios";
+import Spinner from "../components/spinner";
+import { useNavigate } from "react-router-dom";
 
 const CreateBooks = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publishYear, setPulishYear] = useState('');
-  const [loading, setLoading] = useState('');
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const handleSaveBook=()=>{
-    const data ={
+  const handleSaveBook = () => {
+    const data = {
       title,
       author,
-      publishYear
-    }
+      publishYear,
+    };
     setLoading(true);
     axios
-    .post('http://loacalhost:4444/books', data)
-    .then(()=>{
-      setLoading(false);
-      navigate('/');
-    })
-    .catch((error)=>{
-      setLoading(false);
-      alert('something happend please check the console for more infor');
-      console.log(error);
-    })
-  }
+      .post("http://localhost:4444/books", data)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("something happend please check the console for more infor");
+        console.log(error);
+      });
+  };
   return (
-    <div>CreateBooks</div>
-  )
-}
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Create Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4 flex flex-col">
+          <label className="text-xl text-gray-500">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full mt-2"
+          />
+        </div>
+        <div className="my-4 flex flex-col">
+          <label className="text-xl text-gray-500">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full mt-2"
+          />
+        </div>
+        <div className="my-4 flex flex-col">
+          <label className="text-xl text-gray-500">Publish Year</label>
+          <input
+            type="number"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full mt-2"
+          />
+        </div>
+        <button 
+          className="p-2 bg-sky-300 m-8 rounded-lg hover:bg-sky-400 transition-all"
+          onClick={handleSaveBook}
+        >
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default CreateBooks
+export default CreateBooks;
